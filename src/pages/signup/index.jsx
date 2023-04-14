ba37f1a51267a80b5921b861be18ddfb04c47057
rniochy <lniochy@gmail.com>
2023-04-14 06:00:05 +0100

    editing features into singup component

--- src/pages/signup/index.jsx
+++ src/pages/signup/index.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
-import CreateItem from '../../components/createItem';
 import "./signup.css"
 
 const Signup = () => {
-    return <CreateItem />
+    return (
+        <div className='signup-content'>
+            <h2>Welcome to CodeLeap network!</h2>
+            <div>
+                <label htmlFor='button'>Please enter your username</label>
+                <input  id="button"/>
+                <button >ENTER</button>
+            </div>
+        </div>
+    );
 }
 
 export default Signup;
